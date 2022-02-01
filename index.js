4b10ff7f8a4a349df9432fbcf4731e56fb7436ec
martin <martinovington@msn.com>
2022-02-01 08:44:43 +0000

    ex3.6

--- index.js
+++ index.js
@@ -64,9 +64,17 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
+    if (!body.name) {
         return response.status(400).json({
-            error: 'name or number missing'
+            error: 'name is missing'
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number is missing'
+        })
+    } else if (persons.find(person => person.name == body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
         })
     }
 
