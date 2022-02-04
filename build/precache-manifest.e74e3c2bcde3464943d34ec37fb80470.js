ef07ddd67ca04c80915f6a2392528fa4626a6add
Martin Ovington <MartinOvington@msn.com>
2022-02-04 08:15:02 +0000

    added frontend build

---
+++ build/precache-manifest.e74e3c2bcde3464943d34ec37fb80470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac154b7e7b5fb4ba82831e973c0b87b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5939e84ac0b0949720dd",
+    "url": "/static/css/main.d7f09e0b.chunk.css"
+  },
+  {
+    "revision": "309ce805c384bcac96f4",
+    "url": "/static/js/2.ceb6bf3f.chunk.js"
+  },
+  {
+    "revision": "5939e84ac0b0949720dd",
+    "url": "/static/js/main.6b779cf7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
