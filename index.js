5efa13c25c776a319a873e2e88729996bad03aa0
Martin Ovington <MartinOvington@msn.com>
2022-02-08 12:00:15 +0000

    finished ex3.18

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ let persons =
     }
 ]
 
-const infoPage = () => (
+const infoPage = (numEntries) => (
     `<!DOCTYPE html>
     <div>
-        <p>Phonebook has info for ${persons.length} people</p>
+        <p>Phonebook has info for ${numEntries} people</p>
         <p>${Date()}</p>
     </div>`
 )
@@ -56,21 +56,33 @@ app.get('/api/persons', (request, response) => {
     })
   })
 
-  app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
-    })
-  })
-
-app.get('/info', (request, response) => {
-    response.send(infoPage())
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id != id)
 
-    response.status(204).end()
+app.get('/info', (request, response) => {
+    Person.find({})
+        .then(persons => {
+            response.send(infoPage(persons.length))
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -100,6 +112,33 @@ app.post('/api/persons', (request, response) => {
         response.json(savedNote)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
